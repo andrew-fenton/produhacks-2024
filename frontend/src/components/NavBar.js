0e3c95db080ab196fd5ed9ef9a7b430b7b3cf22e
khoaalski <kbsch28@gmail.com>
2024-03-23 15:54:11 -0700

    SEND HELP

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../style/NavBar.css';
+
+function NavBar() {
+
+  return (
+    <div className="navBarContainer">
+            <h1>MOCKUP NAME</h1>
+    </div>
+  );
+}
+
+export default NavBar;
