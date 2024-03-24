0c9f2168014a6bc370acbbb9579882692c397307
khoaalski <kbsch28@gmail.com>
2024-03-23 22:39:33 -0700

    like title, please do props

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import MapObject from './components/MapObject';
 import NavBar from './components/NavBar';
-import BarChart from './components/BarChart';
+// import BarChart1 from './components/BarChart1';
+import { BarChart } from '@mui/x-charts/BarChart';
 
 function App() {
   const [clickLocation, setClickLocation] = useState("");
@@ -16,15 +17,39 @@ function App() {
       <NavBar />
       <div className="contentContainer">
         <div className="barChartContainer">
-        <BarChart
-        customDataset={{
-          data: [18, 15, 20, 26], // Example data values for "This City", "Neighbor Average", "Province Average", "National Average"
-        }}
-        customLabels={['This City', 'Neighbor Average', 'Province Average', 'National Average']}
-        chartTitle="Public Park & Green Spaces Comparison"
-        />
+          {/* <BarChart1
+            customDataset={{
+              data: [18, 15, 20, 26], // Example data values for "This City", "Neighbor Average", "Province Average", "National Average"
+            }}
+            customLabels={['This City', 'Neighbor Average', 'Province Average', 'National Average']}
+            chartTitle="Public Park & Green Spaces Comparison"
+          /> */}
+          <div style={{ width: '100%' }}>
+            <BarChart
+              xAxis={[{ scaleType: 'band', data: ['This City', 'Neighbor Average', 'Province Average', 'National Average'] }]}
+              series={[
+                { data: [4, 8, 9, 3] }
+              ]}
+              width={650}
+              height={400}
+              sx={{
+                "& .MuiBarElement-root:nth-child(1)": {
+                  fill: "#3498DB", // Blue
+                },
+                "& .MuiBarElement-root:nth-child(2)": {
+                  fill: "#2ECC71", // Green
+                },
+                "& .MuiBarElement-root:nth-child(3)": {
+                  fill: "#E67E22", // Orange
+                },
+                "& .MuiBarElement-root:nth-child(4)": {
+                  fill: "#9B59B6", // Purple
+                }
+              }}
+            />
+          </div>
         </div>
-          <MapObject setClickLocation={setClickLocation} className="map" />
+        <MapObject setClickLocation={setClickLocation} className="map" />
       </div>
     </div>
   );
