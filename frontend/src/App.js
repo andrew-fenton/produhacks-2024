91c9c8218590970b8fb67e9be68a3729fac162b9
andrew-fenton <andrewfenton898@gmail.com>
2024-03-24 08:18:43 -0700

    change data on click, stacked chart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import MapObject from './components/MapObject';
 import NavBar from './components/NavBar';
-// import BarChart1 from './components/BarChart1';
 import { BarChart } from '@mui/x-charts/BarChart';
- import LineChart from './components/LineChart';
-import LineGraph from './components/LineGraph';
+// import LineGraph from './components/LineGraph';
  
 
 function App() {
   const [clickLocation, setClickLocation] = useState("");
+
+  function getRandomNum(max) {
+    return Math.floor(Math.random() * max);
+  }
 
   useEffect(() => {
     console.log(clickLocation);
@@ -25,27 +27,28 @@ function App() {
     "White Rock": [8, 19, 20, 26],
   }
 
+  const stackedChartData = [
+    [8, 6, 7, 10],
+    [4, 3, 1, 5],
+    [4, 2, 5, 4],
+    [2, 8, 1, 3]
+  ];
+
   return (
     <div className="app-container">
       <NavBar />
       <div className="contentContainer">
-        <div className="barChartContainer">
-          {/* <BarChart1
-            customDataset={{
-              data: [18, 15, 20, 26], // Example data values for "This City", "Neighbor Average", "Province Average", "National Average"
-            }}
-            customLabels={['This City', 'Neighbor Average', 'Province Average', 'National Average']}
-            chartTitle="Public Park & Green Spaces Comparison"
-          /> */}
-          <div style={{ width: '100%' }}>
-            {clickLocation &&
+        <div className="chartContainer">
+        {clickLocation &&
+          <>
+            <h2>Presence of Public Parks and Green Spaces</h2>
             <BarChart
               xAxis={[{ scaleType: 'band', data: [clickLocation, 'Neighbor Average', 'Province Average', 'National Average'] }]}
               series={[
                 { data: barChartData[clickLocation] }
               ]}
-              width={500}
-              height={300}
+              width={650}
+              height={400}
               sx={{
                 "& .MuiBarElement-root:nth-child(1)": {
                   fill: "#3498DB", // Blue
@@ -61,9 +64,26 @@ function App() {
                 }
               }}
             />
-            }
-            <LineGraph />
-          </div>
+            {/* <LineGraph /> */}
+            <h2>Presence of Recreational Facilities</h2>
+            <BarChart
+              xAxis={[
+                  {
+                    scaleType: 'band',
+                    data: ['This City', 'Neighbor Average', 'Province Average', 'National Average']
+                  }
+              ]}
+              series={[
+                { data: stackedChartData[getRandomNum(stackedChartData.length - 1)], stack: 'A', label: 'Community Centers', color: '#FFD700' },
+                { data: stackedChartData[getRandomNum(stackedChartData.length - 1)], stack: 'A', label: 'Gyms', color: '#FF5733' },
+                { data: stackedChartData[getRandomNum(stackedChartData.length - 1)], stack: 'A', label: 'Swimming Pools', color: '#00BFFF' },
+                { data: stackedChartData[getRandomNum(stackedChartData.length - 1)], stack: 'A', label: 'Sport Complexes', color: '#32CD32' },
+              ]}
+              width={650}
+              height={400}
+            />
+          </>
+        }
         </div>
         <MapObject setClickLocation={setClickLocation} className="map" />
       </div>
