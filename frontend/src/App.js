e8472338f04648bfe3cb1d2c572d0d69ba62c6c2
Andrew Fenton <andrewfenton898@gmail.com>
2024-03-23 21:18:58 -0700

    feat: map locations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import MapObject from './components/MapObject';
 import NavBar from './components/NavBar';
 import BarChart from './components/BarChart';
 
 function App() {
+  const [clickLocation, setClickLocation] = useState("");
+
+  useEffect(() => {
+    console.log(clickLocation);
+  }, [clickLocation]);
+
   return (
     <div className="app-container">
       <NavBar />
       <BarChart
-  customDataset={{
-    data: [18, 15, 20, 26], // Example data values for "This City", "Neighbor Average", "Province Average", "National Average"
-  }}
-  customLabels={['This City', 'Neighbor Average', 'Province Average', 'National Average']}
-  chartTitle="Public Park & Green Spaces Comparison"
-/>
-    <MapObject />
+        customDataset={{
+          data: [18, 15, 20, 26], // Example data values for "This City", "Neighbor Average", "Province Average", "National Average"
+        }}
+        customLabels={['This City', 'Neighbor Average', 'Province Average', 'National Average']}
+        chartTitle="Public Park & Green Spaces Comparison"
+      />
+    <MapObject setClickLocation={setClickLocation} />
     </div>
   );
 }
--- frontend/src/components/DataContainer.js
+++ frontend/src/components/DataContainer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function DataContainer() {
     return (
         <div className='dataContainer'>
-            <p>HEllo</p>
+            <p></p>
         </div>
     );
 }
